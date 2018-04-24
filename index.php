6d1bede22f19350955b4d1e67b2919b47ef5c024
Miguel <miguelfieira@hotmail.com>
2018-04-24 10:37:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-av
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Eric de Haar, Sam van der Greft en Miguel Fieira">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <meta name="description" content="Hotel Calefornia Official Website"/>
+    <title>Hotel Calefornia</title>
+</head>
+<body>
+  <div id="page-wrapper">
+
+  </div>
+</body>
+</html>
+
+<!-- Dit is een Basic html bestand deze kun je copy pasten -->
